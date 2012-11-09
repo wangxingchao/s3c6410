9fe36ef9b313a0e848012e13b1f21f51019e4678
Wang Xingchao <xingchao.wang@intel.com>
2012-11-09 10:19:45 +0800

    SPI: FPGA - spi-fgpa drvier function support
    1) Add read/write fpga support.
    2) Test read, Addr 0x0-->0xaa55
    add loop for test
    
    TODO:
    1) Add debug interface
    2) Add char device interface
    3) Add translation code from FPGA data to real value
    
    Signed-off-by: Wang Xingchao <xingchao.wang@intel.com>

--- drivers/mtd/devices/spifpga.c
+++ drivers/mtd/devices/spifpga.c
@@ -50,6 +50,7 @@
 
 #define CMD(l, h) (l&CMD_MASK | ((h&CMD_MASK)<<4))
 
+static struct spi_device *spi_fpga;
 static int read_fpga(u16 addr, struct spi_dev *spi)
 {
 	u32 code;
@@ -65,13 +66,49 @@ static int read_fpga(u16 addr, struct spi_dev *spi)
 static int write_fpga(u16 addr, u16 val, struct spi_dev *spi)
 {
 	u32 code;
-	u16 val;
 	ssize_t retval;
 
 	code = (addr | (1<<16)) << 16; 
 	code |= val;
 	spi_write(spi, &code, 1);
 	return 0;
+}
+
+static int spi_measure_data(u8 addr)
+{
+	u32 value;
+	u16 address = 0;
+	int retval;
+
+	address |= addr;
+	value = read_fpga(address, spi_fpga);
+	value = value & DATA_MASK; 
+	return value;
+}
+
+static void spi_u14_measure(void)
+{
+	u32 value;
+	u16 addr;
+	int retval;
+
+	addr = 1;
+	//retval = spi_write_then_read(spi_fpga, &code, 1, &val, 1);
+	value = read_fpga(addr, spi_fpga);
+	value = value & DATA_MASK; 
+	printk(KERN_INFO "FPGA: read U14 measure data: 0x%x\n", value);
+}
+static void spi_test_aa55(void)
+{
+	u32 value=0;
+	u16 addr;
+	int retval;
+
+	addr = 0;
+	//retval = spi_write_then_read(spi_fpga, &code, 1, &val, 1);
+	value = read_fpga(addr, spi_fpga);
+	value = value & DATA_MASK; 
+	printk(KERN_INFO "FPGA: read aa55 value: 0x%x\n", value);
 }
 
 /*
@@ -81,16 +118,27 @@ static int write_fpga(u16 addr, u16 val, struct spi_dev *spi)
  */
 static int __devinit fpga_probe(struct spi_device *spi)
 {
-	const struct spi_device_id	*id = spi_get_device_id(spi);
 	struct flash_platform_data	*data;
-	struct m25p			*flash;
 	struct flash_info		*info;
 	unsigned			i;
-	struct mtd_partition		*parts = NULL;
-	int				nr_parts = 0;
+	int ret;
 
+	spi_fpga = spi;
 	printk(KERN_INFO "SPI: Probe SPI FPGA\n");
 
+#if 0
+	/* Initialize SPI GPIO for FPGA */
+	ret = gpio_request(S3C64XX_GPC(3), "fpga");
+	if (ret) {
+		printk(KERN_ERR "SPI: Setup CS-Pin0 Error\n");
+	}
+	s3c_gpio_setpull(S3C64XX_GPC(3), S3C_GPIO_PULL_NONE);	// Manual chip select pin as used in 6410_set_cs
+	s3c_gpio_cfgpin(S3C64XX_GPC(3), S3C_GPIO_OUTPUT);		// Manual chip select pin as used in 6410_set_cs
+#endif
+
+	for (i=0; i<100; i++)
+		spi_test_aa55();
+	spi_u14_measure();
 	return 0;
 }
 
